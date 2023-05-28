79323135593a04b29fa9009e4a8316fc34930a58
rodro277 <rodro@DESKTOP-BPGAS2J>
2023-05-27 21:36:44 -0600

    third day, course finished

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    #ex /polls/
+    path("", views.IndexView.as_view(), name="index"),
+    #ex /polls/#/
+    path("<int:pk>/detail/mejorpagina", views.DetailView.as_view(), name="detail"),  #manera que nos da django
+    #ex: /polls/#/results
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),  #para pasar pararametro variables
+    #ex: 
+    path("<int:question_id>/vote/", views.vote, name="vote"),  #mediante url 
 ]
